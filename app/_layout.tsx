1903659b7f6d39b2c712ecd38007a2451caebde9
Barry hamid <barryhamid0523@gmail.com>
2026-04-29 01:07:06 +0000

    Continuer sans compte and scan screen index or Acceuil for navigate to screen scan

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,23 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="CodeConfirmation" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       
-      <Stack.Screen 
-        name="SignalerIncidentScreen" 
-        options={{ 
-          presentation: 'modal', // Animation de bas en haut
-          headerShown: false,
-          gestureEnabled: true // Permet de fermer en glissant vers le bas
-        }} 
-      />
+        <Stack.Screen 
+          name="SignalerIncidentScreen" 
+          options={{ 
+            presentation: 'modal', // Animation de bas en haut
+            headerShown: false,
+            gestureEnabled: true // Permet de fermer en glissant vers le bas
+          }} 
+        />
+        <Stack.Screen 
+          name="(tabs)/scan" 
+          options={{ 
+            headerShown: false,
+            title: 'Scanner un incident' 
+          }} 
+        />
     </Stack>
   );
 }
